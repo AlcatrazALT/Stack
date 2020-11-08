e7b6fd14e7c34c82b0200c54a932c2241852807a
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-11-08 13:22:18 +0700

    Add moving cube

---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
+
+    [SerializeField]
+    private float _moveSpeed = 1f;
+
+    private void OnEnable()
+    {
+        if (LastCube == null)
+        {
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+        }
+        CurrentCube = this;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    internal void Stop()
+    {
+        _moveSpeed = 0;
+        var hangover = transform.position.z - LastCube.transform.position.z;
+
+        var direction = hangover > 0 ? 1f : -1f;
+        SplitCubeOnZ(hangover, direction);
+    }
+
+    private void SplitCubeOnZ(float hangover, float direction)
+    {
+        var newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
+        var fallingBlockSize = transform.localScale.z - newZSize;
+
+        var newZPosition = LastCube.transform.position.z + (hangover / 2);
+
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
+
+        var cubeEdge = transform.position.z + (newZSize / 2f * direction);
+        var fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f * direction;
+
+        SpawnDropCube(fallingBlockZPosition, fallingBlockSize);
+    }
+
+    private void SpawnDropCube(float fallingBlockZPosition, float fallingBlockSize)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, fallingBlockSize);
+        cube.transform.position = new Vector3(transform.position.x, transform.position.y, fallingBlockZPosition);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * _moveSpeed;
+    }
+}
